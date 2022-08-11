6000957a943495e37e68b7570771b753e4924931
Siverena <vpupkin@company.com>
2022-08-11 10:38:23 +0300

    small fix

--- src/components/dl-user-contents/dl-user-contracts/dl-user-contract-detail/dl-user-contract-detail.js
+++ src/components/dl-user-contents/dl-user-contracts/dl-user-contract-detail/dl-user-contract-detail.js
@@ -1,4 +1,13 @@
 export default {
   name: 'DlUserContractDetail',
   props: ['contract'],
+  computed: {
+    сontractSum() {
+      let sum = 0;
+      this.contract.items.forEach((element) => {
+        sum += element.item.price;
+      });
+      return sum;
+    },
+  },
 };
--- src/pages/PersonalPage/personal-page.js
+++ src/pages/PersonalPage/personal-page.js
@@ -512,6 +512,50 @@ export default {
               },
             ],
           },
+          {
+            id: 106158,
+            status: 2,
+            item: {
+              id: 4031,
+              name: 'Azure Shroud',
+              market_hash_name: 'Azure Shroud',
+              color: '4b69ff',
+              image:
+                '-9a81dlWLwJ2UUGcVs_nsVtzdOEdtWwKGZZLQHTxDZ7I56KW1Zwwo4NUX4oFJZEHLbXK9QlSPcUhogJKXk3ET9ug1sjWHFxuMBZSpbK3JBJkg6CHcCdX_tmlnJKEgvOkYPXQwjgAuZN0076Y9Nn20AKw_RBlZmv7IIfEewFtaQmC8wK4w-i918S1ot2XntWmjJBK',
+              price: 15,
+              type: 'item',
+            },
+            items: [
+              {
+                id: 6451,
+                status: 2,
+                item: {
+                  id: 7,
+                  name: 'Blade of Eternal Reign',
+                  market_hash_name: 'Blade of Eternal Reign',
+                  color: '4b69ff',
+                  image:
+                    '-9a81dlWLwJ2UUGcVs_nsVtzdOEdtWwKGZZLQHTxDZ7I56KW1Zwwo4NUX4oFJZEHLbXK9QlSPcUluxpUWETvTO2j0IDWR1hmKwVbibipKgNlnPbcdC9M7dCJloyKk_K6MOKBwD9VupJw3b_Cooj2jAa2-xE5Z2H3JISSJARvMg6E8lXolO27jIj84sqxN-rEqQ',
+                  price: 1,
+                  type: 'item',
+                },
+              },
+              {
+                id: 27992,
+                status: 5,
+                item: {
+                  id: 514,
+                  name: 'Plagueroad Apothacary',
+                  market_hash_name: 'Plagueroad Apothacary',
+                  color: '4b69ff',
+                  image:
+                    '-9a81dlWLwJ2UUGcVs_nsVtzdOEdtWwKGZZLQHTxDZ7I56KW1Zwwo4NUX4oFJZEHLbXQ5BhMYY49vRpiRVjVStum0srDVk9LKgJoprakLBJl7PbbYjhM79n4xoTcx6Lyar-IkzgGucYm37-UoY7wiQW3rxZtZj2mItTEIwNvYw3VqE_-n7n0AIGp1g',
+                  price: 37,
+                  type: 'item',
+                },
+              },
+            ],
+          },
         ],
         gift: {
           used: true,
